fc56b230679a4f28c6e83bd8bbcaa4a37206dfd4
ChrisAchinga <achinga.chris@gmail.com>
2021-01-30 17:13:12 +0300

    string reverse

---
+++ source/strings-reverse/js/sol1.js
@@ -0,0 +1,9 @@
+function reverseString(string) {
+  var reversedString = string.split('').reverse().join('')
+  return reversedString
+}
+
+output = reverseString('Komba')
+
+console.log(output)
+// 34807324
---
+++ source/strings-reverse/python/sol1.py
@@ -0,0 +1,7 @@
+def strRev (text):
+    return text[::-1]
+    
+X =strRev("Hello")
+print(X)
+y =strRev("Greetings from earth")
+print(y)
